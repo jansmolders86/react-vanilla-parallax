ce9f8736265e743f3019e82c6f5bafd45eb6c385
Jan Smolders <jansmolders86@gmail.com>
2018-06-11 21:08:34 +0200

    added correct imagery

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Parallax from './Parallax'
 class App extends Component {
     render() {
         const images = [
-            'https://raw.githubusercontent.com/jansmolders86/react-3D-parallax-card/master/public/images/bg.jpg',
-            'https://raw.githubusercontent.com/jansmolders86/react-3D-parallax-card/master/public/images/front.png'
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer7.jpg',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer6.png',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer5.png',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer4.png',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer3.png',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer2.png',
+            'https://raw.githubusercontent.com/jansmolders86/react-vanilla-parallax/master/public/images/layer1.png',
+
         ];
 
         return (
